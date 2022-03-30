d39d50b537ebb07b2d548fdf96c1737a7bb1373b
Daniel Collishaw <daniel.collishaw@icloud.com>
2022-03-30 13:29:37 -0400

    Added dbms functionality

--- server/index.js
+++ server/index.js
@@ -1,23 +1,44 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Setting up GET for local API
+/*
+This is the how to connect the dbms
+
+const mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: "kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "f5gigm2mf017jybo",
+  password: "zhrz58cvoiymdzai",
+  database: "rizjyg7r4b89vtwx"
+});
+
+connection.connect();
+
+This is how to query, it will return a list of rows so you can access using
+index, each row is a json object.
+
+connection.query("SELECT * FROM test", (err, res) => {
+  if (err) throw err;
+
+  console.log(res);
+});
+*/
+
+// API Calls
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from Express!"})
 });
 
-// Open app to run on local PORT
 app.listen(PORT, () => {
   console.log("Server listening on " + PORT);
 });
 
-// Heroku deployment
-if(process.env.NODE_ENV === 'production'){
+// Defines Heroku deployment
+if (process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('client/build'));
 }
-app.get('*',(req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
